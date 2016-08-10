3c17fc13ed633fc3cd6e9e318207becdf6c69d94
王正一 <fsymbio@icloud.com>
2016-08-10 17:22:25 +0800

    创建基本雏形

---
+++ FSNetwork/FSNetwork/BackendConfig.swift
@@ -0,0 +1,13 @@
+//
+//  BackendConfig.swift
+//  FSNetwork
+//
+//  Created by 王正一 on 16/8/10.
+//  Copyright © 2016年 FsThatOne. All rights reserved.
+//
+
+import UIKit
+
+class BackendConfig: Object {
+
+}
